710a22dde4d5c80660963f5d547e24a897be0131
P <mp24@LAMU02ZJ407LVCF.uhc.com>
2021-04-01 18:17:44 +0530

    Code cleanup

--- swap.c
+++ swap.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
- int a=51;
- int b=2;
+  int a=51;
+  int b=2;
 
- //Swapping...
- int temp=a;
- a=b;
- b=temp;
+  //Swapping...
+  int temp=a;
+  a=b;
+  b=temp;
 
- printf("After swapping , a is %d , b is %d",a,b);
- return 0;
+  printf("After swapping , a is %d , b is %d",a,b);
+  return 0;
 }
